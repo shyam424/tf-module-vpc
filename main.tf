59b5cb80b828e2b569daa7e8733009fc74f33bdf
Shyam <shyamneo24@gmail.com>
2023-11-02 13:56:50 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
+  tags = merge (local.tags, {Name = "${var.env}-vpc"}}
 }
 
 module "subnets" {
@@ -7,15 +8,14 @@ module "subnets" {
   for_each = var.subnets
   subnets = each.value
   vpc_id = aws_vpc.main.id
+  tags = local.tags
+  env = var.env
 }
 
 
 resource "aws_internet_gateway" "igw" {    #internet gateway creation
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
+  tags = merge (local.tags, {Name = "${var.env}-igw"}}
 }
 
 resource "aws_route" "igw" {
@@ -39,6 +39,7 @@ resource "aws_nat_gateway" "ngw" {
   count                     = length(local.public_subnet_ids)
   allocation_id             = element(aws_eip.ngw.*.id, count.index)
   subnet_id                 = element(local.public_subnet_ids, count.index)
+  tags = merge (local.tags, {Name = "${var.env}-ngw"}}
 }
 
 #Nat gateways are creating and pulling the elastic IPs from the above
@@ -57,6 +58,7 @@ resource "aws_vpc_peering_connection"  "peering" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
   auto_accept   = true
+  tags = merge (local.tags, {Name = "${var.env}-peer"}}
 }
 #peering connection
 
